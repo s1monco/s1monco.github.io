0b9863e18c463ea7167233fc021bff98596888f4
Corentin <corentin.simon@cryptio.co>
2024-02-05 19:46:45 -0300

    lalal

--- script/main.js
+++ script/main.js
@@ -4,9 +4,9 @@ const animationTimeline = () => {
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
   const hbd = document.getElementsByClassName("wish-hbd")[0];
 
-  textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
+  //textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
     //.split("")
-    .join("</span><span>")}</span`;
+   // .join("</span><span>")}</span`;
 
   hbd.innerHTML = `<span>${hbd.innerHTML
     .split("")
